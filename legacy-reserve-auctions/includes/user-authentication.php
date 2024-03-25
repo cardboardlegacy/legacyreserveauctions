e4c634ec099c8746b886272a3e6d11b4dcc72974
cardboardlegacy <164824007+cardboardlegacy@users.noreply.github.com>
2024-03-24 22:30:01 -0500

    Update user-authentication.php

--- legacy-reserve-auctions/includes/user-authentication.php
+++ legacy-reserve-auctions/includes/user-authentication.php
@@ -1,1 +1,24 @@
+<?php
+// Check if the user is logged in
+function is_user_logged_in() {
+    return is_user_logged_in();
+}
 
+// Get the current user's ID
+function get_current_user_id() {
+    return get_current_user_id();
+}
+
+// Get the current user's display name
+function get_current_user_display_name() {
+    $user = wp_get_current_user();
+    return $user->display_name;
+}
+
+// Check if the user has placed a bid on the current auction product
+function has_user_placed_bid($product_id) {
+    $user_id = get_current_user_id();
+    // TODO: Implement the logic to check if the user has placed a bid on the specified product
+    // This will be covered in the bidding system functionality
+    return false;
+}
