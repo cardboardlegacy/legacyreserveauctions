454fdaa2a63eb0d9de81362ba684064cf7cf501f
cardboardlegacy <164824007+cardboardlegacy@users.noreply.github.com>
2024-03-24 23:35:11 -0500

    Update single-product-auction.php

--- legacy-reserve-auctions/templates/single-product-auction.php
+++ legacy-reserve-auctions/templates/single-product-auction.php
@@ -1,1 +1,40 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
 
+get_header('shop');
+
+while (have_posts()) : the_post();
+    global $product;
+
+    if ($product->get_type() !== 'auction') {
+        wc_get_template_part('content', 'single-product');
+        continue;
+    }
+    ?>
+
+    <div id="product-<?php the_ID(); ?>" <?php wc_product_class('', $product); ?>>
+        <?php
+        do_action('woocommerce_before_single_product');
+
+        if (post_password_required()) {
+            echo get_the_password_form();
+            return;
+        }
+        ?>
+
+        <div class="summary entry-summary">
+            <?php do_action('woocommerce_single_product_summary'); ?>
+        </div>
+
+        <?php
+        do_action('woocommerce_after_single_product_summary');
+        ?>
+    </div>
+
+    <?php do_action('woocommerce_after_single_product'); ?>
+
+<?php endwhile;
+
+get_footer('shop');
